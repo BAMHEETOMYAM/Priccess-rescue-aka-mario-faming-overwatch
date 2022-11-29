3ab920975eb944a77acc72e7c66348068e0caab6
Jirateepatom <Jirateep.tana@bumail.net>
2022-11-29 20:00:27 +0700

    some name

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('atomtoey888')
+    print_hi('some name')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
